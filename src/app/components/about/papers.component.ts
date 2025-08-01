2423676555b30e38229ce9a28791e3edeea4bf52
saif-abualkhair <saif_abualkhair@hotmail.com>
2025-08-02 00:26:12 +0300

    refactor: refactored the about page to the papers page instead

---
+++ src/app/components/about/papers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clp-papers',
+  imports: [],
+  templateUrl: './papers.component.html',
+  styleUrl: './papers.component.scss'
+})
+export class PapersComponent {
+
+}
